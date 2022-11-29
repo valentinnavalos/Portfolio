b65df6082db98a465e4283d58e0677a873276d95
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-11-29 17:31:13 -0300

    refactor: update about

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -11,86 +11,84 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
   return (
-    <Container fluid className="home-about-section" id="about">
+    <Container fluid className='home-about-section' id='about'>
       <Container>
         <Row>
-          <Col md={8} className="home-about-description">
+          <Col md={8} className='home-about-description'>
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              LET ME <span className='purple'> INTRODUCE </span> MYSELF
             </h1>
-            <p className="home-about-body">
+            <p className='home-about-body'>
               I fell in love with programming and I have at least learnt
               something, I think… 🤷‍♂️
               <br />
               <br />I have long experience building with
               <i>
-                <b className="purple"> Javascript. </b>
+                <b className='purple'> JavaScript</b> and{" "}
+                <b className='purple'>TypeScript</b>.
               </i>
               <br />
-              <br />
-              I have fun learning new technologies, such as &nbsp;
+              <br />I have fun learning new technologies, such as &nbsp;
               <i>
-                <b className="purple">Solidity and Smart Contracts </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain Development.
-                </b>
+                <b className='purple'>Solidity and Smart Contracts </b> and also
+                in areas related to{" "}
+                <b className='purple'>Blockchain Development.</b>
               </i>
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className='purple'>Node.js</b> and
               <i>
-                <b className="purple">
+                <b className='purple'>
                   {" "}
                   Modern Javascript Library and Frameworks
                 </b>
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js</b>
+                <b className='purple'> React.js</b>
               </i>
             </p>
           </Col>
-          <Col md={4} className="myAvtar">
+          <Col md={4} className='myAvtar'>
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} className='img-fluid' alt='avatar' />
             </Tilt>
           </Col>
         </Row>
         <Row>
-          <Col md={12} className="home-about-social">
+          <Col md={12} className='home-about-social'>
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className='purple'>connect </span>with me
             </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
+            <ul className='home-about-social-links'>
+              <li className='social-icons'>
                 <a
-                  href="https://github.com/valentinnavalos"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  href='https://github.com/valentinnavalos'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour  home-social-icons'
                 >
                   <AiFillGithub />
                 </a>
               </li>
-              <li className="social-icons">
+              <li className='social-icons'>
                 <a
-                  href="https://twitter.com/buustedbylearnn"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  href='https://twitter.com/valennavalos'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour  home-social-icons'
                 >
                   <AiOutlineTwitter />
                 </a>
               </li>
-              <li className="social-icons">
+              <li className='social-icons'>
                 <a
-                  href="https://www.linkedin.com/in/valentinnavalos/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  href='https://www.linkedin.com/in/valentinnavalos/'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour  home-social-icons'
                 >
                   <FaLinkedinIn />
                 </a>
